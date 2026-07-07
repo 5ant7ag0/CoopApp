ae465e1da271ca2062d2f530d6c6b11eabab4f72
Santiago Tenensaray <santytenensaray@gmail.com>
2026-07-06 22:38:35 -0500

    Fix V18 migration by dropping constraint before creating it

--- src/main/resources/db/migration/V18__isolate_usuarios_admin_identificacion.sql
+++ src/main/resources/db/migration/V18__isolate_usuarios_admin_identificacion.sql
@@ -1,2 +1,3 @@
 ALTER TABLE usuarios_admin DROP CONSTRAINT IF EXISTS uk_usuarios_admin_identificacion;
+ALTER TABLE usuarios_admin DROP CONSTRAINT IF EXISTS uk_empresa_identificacion_admin;
 ALTER TABLE usuarios_admin ADD CONSTRAINT uk_empresa_identificacion_admin UNIQUE (empresa_id, identificacion);
